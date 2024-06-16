79205207a165650f903b4ed6419bd5e479e05662
Alexander Chapchuk <BigTows@yandex.ru>
2024-06-16 11:56:56 +0300

    //

--- src/pages/forecast/page.tsx
+++ src/pages/forecast/page.tsx
@@ -21,6 +21,7 @@ import {
   useCreateForecastReportMutation,
   useLazyGetReportQuery,
 } from '../../store/api/export/exportApi';
+import pencil from '../../assets/pencil.svg';
 
 ChartJS.register(
   CategoryScale,
@@ -79,6 +80,7 @@ export const ForecastPage = () => {
         data.forEach((workload: any) => {
           const key = workload.modality + '_' + workload.typeModality;
           const value: IWorkload = {
+            year: workload.year,
             weekNumber: workload.week,
             manualValue:
               workload.manualValue === null ? NaN : workload.manualValue,
@@ -130,6 +132,7 @@ export const ForecastPage = () => {
       legend: {
         position: 'top' as const,
         align: 'start' as const,
+        padding: 30,
       },
       title: {
         display: false,
@@ -164,6 +167,10 @@ export const ForecastPage = () => {
       },
     ],
   };
+
+  const [editableNumbersOfWeek, setEditableNumbersOfWeek] =
+    useState<number>(NaN);
+
   return (
     <div
       className={
@@ -211,6 +218,95 @@ export const ForecastPage = () => {
           })}
         </div>
         <Line data={data} options={options} />
+      </div>
+      <div className="grid grid-flow-row-dense grid-cols-3 bg-white p-4 rounded shadow">
+        <table className="max-h-3 text-sm text-left col-span-2 overflow-scroll">
+          <thead className="border-b">
+            <tr>
+              <th className="px-4 py-2">Год</th>
+              <th className="px-4 py-2">Неделя</th>
+              <th className="px-4 py-2">Фактич. значения</th>
+              <th className="px-4 py-2">Прогноз</th>
+              <th className="px-4 py-2">Погрешность</th>
+            </tr>
+          </thead>
+          <tbody>
+            {workloadData.get(selectedWorkload)?.map((row) => (
+              <tr key={row.weekNumber} className="border-b">
+                <td className="px-4 py-2">{row.year}</td>
+                <td className="px-4 py-2">{row.weekNumber}</td>
+                <td className="px-4 py-2">
+                  {isNaN(row.manualValue) ? 'Нет данных' : row.manualValue}
+                  <span
+                    onClick={() => setEditableNumbersOfWeek(row.weekNumber)}
+                  >
+                    {' '}
+                    <img src={pencil} alt="pencil" />
+                  </span>
+                </td>
+                <td className="px-4 py-2">
+                  {isNaN(row.generatedValue)
+                    ? 'Нет данных'
+                    : row.generatedValue}
+                </td>
+                <td className="px-4 py-2">
+                  {isNaN(row.generatedValue) || isNaN(row.manualValue)
+                    ? '-'
+                    : row.manualValue - row.generatedValue}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div className="col-span-1">
+          <div className="grid grid-cols-3">
+            {!isNaN(editableNumbersOfWeek) && (
+              <>
+                <div className={`col-span-3`}>
+                  Редактирование фактических значений
+                </div>
+                <div className={`col-span-3`}>
+                  Неделя №{editableNumbersOfWeek}
+                </div>
+                <div className={`col-start-1 col-end-3`}>
+                  <label className="block text-gray-700">
+                    Фактич. значения
+                  </label>
+                  <input
+                    type="number"
+                    value={1}
+                    onChange={(e) => {}}
+                    className="border px-2 py-1 rounded w-full"
+                  />
+                </div>
+                <div className={`col-span-2`}>
+                  <button
+                    onClick={() => {}}
+                    className="px-4 py-2 bg-blue-500 text-white rounded ml-2"
+                  >
+                    Подтвердить изменение
+                  </button>
+                </div>
+                <div>
+                  <button
+                    onClick={() => {
+                      setEditableNumbersOfWeek(NaN);
+                    }}
+                    className="px-4 py-2 bg-gray-300 rounded ml-2"
+                  >
+                    Отменить
+                  </button>
+                </div>
+              </>
+            )}
+            {isNaN(editableNumbersOfWeek) && (
+              <div>
+                Нажмите на редактирование фактических значений, чтобы изменить
+                данные...
+              </div>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/pages/forecast/types.ts
+++ src/pages/forecast/types.ts
@@ -1,4 +1,5 @@
 export interface IWorkload {
+  year: number;
   weekNumber: number;
   manualValue: number;
   generatedValue: number;
